4d99ce3adb2ac9718994580bc2e86355fb4868c7
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-09-23 16:26:05 -0300

    adicionar biblioteca buildable

--- apps/angular-template/src/app/app.module.ts
+++ apps/angular-template/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ImageModule } from '@mr/image';
+import { ButtonModule } from '@mr/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ImageModule],
+  imports: [BrowserModule, ImageModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
