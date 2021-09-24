e52f76a2fafabd5c7f5a547ecf98f14148b51c05
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-09-24 18:02:27 -0300

    remover lib image

--- projects/libs/image/src/lib/image.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ImageComponent } from './image/image.component';
-
-@NgModule({
-  imports: [],
-  declarations: [ImageComponent],
-  exports: [ImageComponent],
-})
-export class ImageModule {}
--- projects/libs/image/src/lib/image/image.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'mr-image',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ImageComponent {}
