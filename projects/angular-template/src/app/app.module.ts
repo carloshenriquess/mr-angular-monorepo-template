a26a66b0bb7713ecc091d30fd4ae1180fe006d08
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-09-24 18:01:21 -0300

    criar biblioteca input

--- projects/angular-template/src/app/app.module.ts
+++ projects/angular-template/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ImageModule } from '@mr/image';
 import { ButtonModule } from '@mr/button';
+import { InputModule } from '@mr/input';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ImageModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    ImageModule,
+    ButtonModule,
+    InputModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
