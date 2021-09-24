fdb7ae18c646d6b0769918182699665a3331d478
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-09-24 15:36:10 -0300

    corrigir erro no build

--- projects/libs/button/src/lib/button.module.ts
+++ projects/libs/button/src/lib/button.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [],
   declarations: [ButtonComponent],
   exports: [ButtonComponent],
 })
-export class ButtonModule {}
+export class ButtonModule { }
